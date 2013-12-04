5d96bb7eab158442af2e297dc1358ea9a66770e3
KeithAsazawa <keith.asazawa@gmail.com>
2013-12-04 14:37:51 -0800

    Edit index

--- index.php
+++ index.php
@@ -6,14 +6,12 @@
 <!--[if gt IE 8]><!--> <html lang="en-US"> <!--<![endif]-->
 <head>
 
-<!-- Meta Tags -->
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
 <title>Keith Asazawa</title>   
 
 <meta name="description" content="Insert Your Site Description" /> 
 
-<!-- Mobile Specifics -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="HandheldFriendly" content="true"/>
 <meta name="MobileOptimized" content="320"/>   
@@ -21,62 +19,22 @@
 <!-- Mobile Internet Explorer ClearType Technology -->
 <!--[if IEMobile]>  <meta http-equiv="cleartype" content="on">  <![endif]-->
 
-<!-- Bootstrap -->
 <link href="_include/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Main Style -->
 <link href="_include/css/main.css" rel="stylesheet">
-
-<!-- Supersized -->
 <link href="_include/css/supersized.css" rel="stylesheet">
 <link href="_include/css/supersized.shutter.css" rel="stylesheet">
-
-<!-- FancyBox -->
 <link href="_include/css/fancybox/jquery.fancybox.css" rel="stylesheet">
-
-<!-- Font Icons -->
 <link href="_include/css/fonts.css" rel="stylesheet">
-
-<!-- Shortcodes -->
 <link href="_include/css/shortcodes.css" rel="stylesheet">
-
-<!-- Responsive -->
 <link href="_include/css/bootstrap-responsive.min.css" rel="stylesheet">
 <link href="_include/css/responsive.css" rel="stylesheet">
-
-<!-- Supersized -->
-<link href="_include/css/supersized.css" rel="stylesheet">
-<link href="_include/css/supersized.shutter.css" rel="stylesheet">
-
-<!-- Google Font -->
 <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900' rel='stylesheet' type='text/css'>
-
-<!-- Fav Icon -->
 <link rel="shortcut icon" href="#">
-
 <link rel="apple-touch-icon" href="#">
 <link rel="apple-touch-icon" sizes="114x114" href="#">
 <link rel="apple-touch-icon" sizes="72x72" href="#">
 <link rel="apple-touch-icon" sizes="144x144" href="#">
-
-<!-- Modernizr -->
 <script src="_include/js/modernizr.js"></script>
-
-<!-- Analytics -->
-<script type="text/javascript">
-
-//   var _gaq = _gaq || [];
-//   _gaq.push(['_setAccount', 'Insert Your Code']);
-//   _gaq.push(['_trackPageview']);
-
-//   (function() {
-//     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-//     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-//     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-//   })();
-
-// </script>
-<!-- End Analytics -->
 
 </head>
 
@@ -93,7 +51,6 @@
                 <li><a href="#work">My Work</a></li>
                 <li><a href="#about">About Me</a></li>
                 <li><a href="#contact">Contact</a></li>
-                <!-- <li><a href="shortcodes.html" class="external">Shortcodes</a></li> -->
             </ul>
         </nav>        
     </div>
@@ -123,9 +80,7 @@
         <a id="nextsection" href="#work"><i class="font-icon-arrow-simple-down"></i></a>
     </div>
 </div>
-<!-- End Homepage Slider -->
 
-<!-- Header -->
 <header>
     <div class="sticky-nav">
     	<a id="mobile-nav" class="menu-nav" href="#menu-nav"></a>
@@ -137,17 +92,13 @@
                 <li><a href="#work">My Work</a></li>
                 <li><a href="#about">About Me</a></li>
                 <li><a href="#contact">Contact</a></li>
-				<!-- <li><a href="shortcodes.html" class="external">Shortcodes</a></li> -->
             </ul>
         </nav>        
     </div>
 </header>
-<!-- End Header -->
 
-<!-- Our Work Section -->
 <div id="work" class="page">
 	<div class="container">
-    	<!-- Title Page -->
         <div class="row">
             <div class="span12">
                 <div class="title-page">
@@ -155,102 +106,62 @@
                 </div>
             </div>
         </div>
-        <!-- End Title Page -->
-            
             <div class="span9 offset1">
             	<div class="row">
                 	<section id="projects">
                     	<ul id="thumbs">                       
-							<!-- Item Project and Filter Name -->
                         	<li class="item-thumbs span3 design">
-                            	<!-- Fancybox - Gallery Enabled - Title - Full Image -->
                             	<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Pinterest Clone" href="_include/img/work/full/full-image-01.png">
                                 	<span class="overlay-img"></span>
                                     <span class="overlay-img-thumb font-icon-plus"></span>
                                 </a>
-                                <!-- Thumb Image and Description -->
-                                <img src="_include/img/work/thumbs/image-01.jpg" alt="I created this Pinterest clone using Ruby on Rails. The image feed is displayed using jQuery Masonry.">
+                                <img src="_include/img/work/thumbs/image-01.jpg" alt="I created this Pinterest clone using Ruby on Rails. The image feed is displayed using jQuery Masonry, while the login/registration is handled with Devise.">
                             </li>
-                        	<!-- End Item Project -->
-                            
-							<!-- Item Project and Filter Name -->
                         	<li class="item-thumbs span3 design">
-                            	<!-- Fancybox - Gallery Enabled - Title - Full Image -->
                             	<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Michael Hartl's Twitter tutorial" href="_include/img/work/full/full-image-02.png">
                                 	<span class="overlay-img"></span>
                                     <span class="overlay-img-thumb font-icon-plus"></span>
                                 </a>
-                                <!-- Thumb Image and Description -->
-                                <img src="_include/img/work/thumbs/image-02.jpg" alt="This was my first attempt at a full blown Ruby on Rails application. I hit many bumps along the way, but in the end was a great learning experience. I highlr recommend.">
+                                <img src="_include/img/work/thumbs/image-02.jpg" alt="This was my first attempt at a full blown Ruby on Rails application. I hit many bumps along the way, but in the end was a great learning experience. I highly recommend.">
                             </li>
-                        	<!-- End Item Project -->
-                            
-							<!-- Item Project and Filter Name -->
                         	<li class="item-thumbs span3 photography">
-                            	<!-- Fancybox - Gallery Enabled - Title - Full Image -->
                             	<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Team Fast Agents" href="_include/img/work/full/full-image-03.png">
                                 	<span class="overlay-img"></span>
                                     <span class="overlay-img-thumb font-icon-plus"></span>
                                 </a>
-                                <!-- Thumb Image and Description -->
                                 <img src="_include/img/work/thumbs/image-03.jpg" alt="Team Fast Agents website. Built on Wordpress with some custom CSS.">
                             </li>
-                        	<!-- End Item Project -->
-                            
-							<!-- Item Project and Filter Name -->
                             <li class="item-thumbs span3 photography">
-                                <!-- Fancybox - Gallery Enabled - Title - Full Image -->
                                 <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="AVO Clear Bra" href="_include/img/work/full/full-image-04.png">
                                     <span class="overlay-img"></span>
                                     <span class="overlay-img-thumb font-icon-plus"></span>
                                 </a>
-                                <!-- Thumb Image and Description -->
                                 <img src="_include/img/work/thumbs/image-04.png" alt="Static HTML website built for AVO Clear Bra in Walnut Creek.">
                             </li>
-                        	<!-- End Item Project -->
-                            
-							<!-- Item Project and Filter Name -->
                             <li class="item-thumbs span3 photography">
-                                <!-- Fancybox - Gallery Enabled - Title - Full Image -->
+                                <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Team Fast Agents Newsletter Headers" href="_include/img/work/full/full-image-05.png">
+                                    <span class="overlay-img"></span>
+                                    <span class="overlay-img-thumb font-icon-plus"></span>
+                                </a>
+                                <img src="_include/img/work/thumbs/image-05.jpg" alt="Some sample weekly Team Fast Agents Newsletter headers created using Photoshop.">
+                            </li>
+                            <li class="item-thumbs span3 photography">
                                 <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="AVO Clear Bra" href="_include/img/work/full/full-image-04.png">
                                     <span class="overlay-img"></span>
                                     <span class="overlay-img-thumb font-icon-plus"></span>
                                 </a>
-                                <!-- Thumb Image and Description -->
                                 <img src="_include/img/work/thumbs/image-04.png" alt="Static HTML website built for AVO Clear Bra in Walnut Creek.">
                             </li>
-                        	<!-- End Item Project -->
-                            
-							<!-- Item Project and Filter Name -->
-                            <li class="item-thumbs span3 photography">
-                                <!-- Fancybox - Gallery Enabled - Title - Full Image -->
-                                <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="AVO Clear Bra" href="_include/img/work/full/full-image-04.png">
-                                    <span class="overlay-img"></span>
-                                    <span class="overlay-img-thumb font-icon-plus"></span>
-                                </a>
-                                <!-- Thumb Image and Description -->
-                                <img src="_include/img/work/thumbs/image-04.png" alt="Static HTML website built for AVO Clear Bra in Walnut Creek.">
-                            </li>
-                        	<!-- End Item Project -->
-                            
-							<!-- Item Project and Filter Name -->
-            
-                        	<!-- End Item Project -->
                         </ul>
                     </section>
                     
             	</div>
             </div>
         </div>
-        <!-- End Portfolio Projects -->
     </div>
 </div>
-<!-- End Our Work Section -->
-
-<!-- About Section -->
 <div id="about" class="page-alternate">
 <div class="container">
-    <!-- Title Page -->
     <div class="row">
         <div class="span12">
             <div class="title-page">
@@ -258,46 +169,29 @@
             </div>
         </div>
     </div>
-    <!-- End Title Page -->
-    
-    <!-- People -->
     <div class="row">
-        <!-- Start Profile -->
+
     	<div class="span6 offset3 text-center">
-        	<div class="image-wrap">
-<!--                 <div class="hover-wrap">
-                    <span class="overlay-img"></span>
-                    <span class="overlay-text-thumb">Web Developer</span>
-                </div> -->
+        	<!-- <div class="image-wrap">
                 <img src="_include/img/profile/profile-01.jpg" alt="John Doe">
-            </div>
+            </div> -->
             <h3 class="profile-name">Keith Asazawa</h3>
-            <p class="profile-description">I am a Web Developer from Oakland, CA.</p>
+            <p class="profile-description">I am a Web Developer born and raised in Oakland, CA. <br />I started learning to code in 2010 out of complete curiousity and fell in love with it. In early 2013 I decided it was time to finally take the jump and make coding my career. I applied for CodingDojo, a 9 week intensive coding bootcamp. During my time there we covered HTML, CSS, PHP, JavaScript, and Ruby on Rails. <br/><br/>Post bootcamp I started working for Michael James Real Estate on their marketing and web development team. During my time there I worked with Wordpress and Photoshop, designing and creating marketing material such as their weekly newsletter headers, flyers, and mailers.<br/><br/></p>
             	
             <div class="social">
             	<ul class="social-icons">
-                	<li><a href="#"><i class="font-icon-social-twitter"></i></a></li>
-                    <li><a href="#"><i class="font-icon-social-facebook"></i></a></li>
+                	<li><a href="http://twitter.com/andkeithsaid"><i class="font-icon-social-twitter"></i></a></li>
+                    <li><a href="http://facebook.com/keith.asazawa"><i class="font-icon-social-facebook"></i></a></li>
                 </ul>
             </div>
         </div>
-        <!-- End Profile -->
-        
-        <!-- Start Profile -->
-
-        <!-- End Profile -->
-        
     </div>
-    <!-- End People -->
 </div>
 </div>
-<!-- End About Section -->
 
-
-<!-- Contact Section -->
 <div id="contact" class="page">
 <div class="container">
-    <!-- Title Page -->
+
     <div class="row">
         <div class="span12">
             <div class="title-page">
@@ -306,34 +200,10 @@
             </div>
         </div>
     </div>
-    <!-- End Title Page -->
-    
-    <!-- Contact Form -->
+
     <div class="row">
-    	<div class="span9">
         
-        	<form id="contact-form" class="contact-form" action="#">
-            	<p class="contact-name">
-            		<input id="contact_name" type="text" placeholder="Full Name" value="" name="name" />
-                </p>
-                <p class="contact-email">
-                	<input id="contact_email" type="text" placeholder="Email Address" value="" name="email" />
-                </p>
-                <p class="contact-message">
-                	<textarea id="contact_message" placeholder="Your Message" name="message" rows="15" cols="40"></textarea>
-                </p>
-                <p class="contact-submit">
-                	<a id="contact-submit" class="submit" href="#">Send Your Email</a>
-                </p>
-                
-                <div id="response">
-                
-                </div>
-            </form>
-         
-        </div>
-        
-        <div class="span3">
+        <div class="span12 text-center">
         	<div class="contact-details">
         		<h3>Contact Details</h3>
                 <ul>
@@ -350,29 +220,10 @@
             </div>
         </div>
     </div>
-    <!-- End Contact Form -->
+
 </div>
 </div>
-<!-- End Contact Section -->
 
-<!-- Twitter Feed -->
-<!-- <div id="twitter-feed" class="page-alternate">
-	<div class="container">
-    	<div class="row">
-            <div class="span12">
-                <div class="follow">
-                    <a href="https://twitter.com/Bluxart" title="Follow Me on Twitter" target="_blank"><i class="font-icon-social-twitter"></i></a>
-                </div>
-                    
-                <div id="ticker" class="query"> 
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
-<!-- End Twitter Feed -->
-
-<!-- Socialize -->
 <div id="social-area" class="page">
 	<div class="container">
     	<div class="row">
@@ -381,7 +232,7 @@
                     <ul>
                         <li><a href="https://twitter.com/andkeithsaid" title="Follow Me on Twitter" target="_blank"><span class="font-icon-social-twitter"></span></a></li>
                         <li><a href="https://www.facebook.com/keithasazawa" title="Follow Me on Facebook" target="_blank"><span class="font-icon-social-facebook"></span></a></li>
-                        <li><a href="https://plus.google.com/105500420878314068694" title="Follow Me on Google Plus" target="_blank"><span class="font-icon-social-google-plus"></span></a></li>
+                        <li><a href="https://google.com/+keithasazawa" title="Follow Me on Google Plus" target="_blank"><span class="font-icon-social-google-plus"></span></a></li>
                         <li><a href="http://www.linkedin.com/in/keithasazawa" title="Follow Me on LinkedIn" target="_blank"><span class="font-icon-social-linkedin"></span></a></li>
                     </ul>
                 </nav>
@@ -389,22 +240,15 @@
         </div>
     </div>
 </div>
-<!-- End Socialize -->
 
-<!-- Footer -->
 <footer>
 	<p class="credits">Keith Asazawa</p>
 </footer>
-<!-- End Footer -->
 
-<!-- Back To Top -->
 <a id="back-to-top" href="#">
 	<i class="font-icon-arrow-simple-up"></i>
 </a>
-<!-- End Back to Top -->
 
-
-<!-- Js -->
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> <!-- jQuery Core -->
 <script src="_include/js/bootstrap.min.js"></script> <!-- Bootstrap -->
 <script src="_include/js/supersized.3.2.7.min.js"></script> <!-- Slider -->
@@ -416,7 +260,6 @@
 <script src="_include/js/jquery.tweet.js"></script> <!-- Tweet -->
 <script src="_include/js/plugins.js"></script> <!-- Contains: jPreloader, jQuery Easing, jQuery ScrollTo, jQuery One Page Navi -->
 <script src="_include/js/main.js"></script> <!-- Default JS -->
-<!-- End Js -->
 
 </body>
 </html>
